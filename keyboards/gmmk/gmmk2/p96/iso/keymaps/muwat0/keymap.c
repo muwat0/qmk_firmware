e53b15c55cfd248ad488ae3369a45a90ec3041d4
murat <murat@muwat.org>
2026-07-09 04:27:36 +0300

    set mod-tap key bindings

--- keyboards/gmmk/gmmk2/p96/iso/keymaps/muwat0/keymap.c
+++ keyboards/gmmk/gmmk2/p96/iso/keymaps/muwat0/keymap.c
@@ -14,12 +14,25 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+// muwat's Glorious GMMK 2 %96 ISO Keyboard QMK Configuration
+
 #include QMK_KEYBOARD_H
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 #define _BL 0
 #define _FL 1
+
+// HELD Bindings (Mod-Tap)
+#define HOME_A LSFT_T(KC_A)  // A acts as Left Shift
+#define HOME_S LCTL_T(KC_S)  // S acts as Left Ctrl
+#define HOME_D LGUI_T(KC_D)  // D acts as Left GUI
+#define HOME_F LALT_T(KC_F)  // F acts as Left Alt
+
+#define HOME_J RALT_T(KC_J)  // J acts as Right Alt
+#define HOME_K RGUI_T(KC_K)  // K acts as Right GUI
+#define HOME_L RCTL_T(KC_L)  // L acts as Right Ctrl
+#define HOME_SCLN RSFT_T(KC_SCLN)  // ; acts as Right Shift
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: Base Layer (Default Layer)
@@ -28,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GESC,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_DEL,   KC_INS,   KC_PGUP,  KC_PGDN,
   KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_NLCK,  KC_PSLS,  KC_PAST,  KC_PMNS,
   KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_ENT,   KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
-  KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,            KC_P4,    KC_P5,    KC_P6,
+  KC_CAPS,  HOME_A,     HOME_S,     HOME_D,     HOME_F,     KC_G,     KC_H,     HOME_J,     HOME_K,     HOME_L,     HOME_SCLN,  KC_QUOT,  KC_NUHS,            KC_P4,    KC_P5,    KC_P6,
   KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    KC_P1,    KC_P2,    KC_P3,    KC_PENT,
   KC_LCTL,  KC_LGUI,  KC_LALT,                      KC_SPC,                                 KC_RALT,  MO(_FL),  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,    KC_PDOT),
 

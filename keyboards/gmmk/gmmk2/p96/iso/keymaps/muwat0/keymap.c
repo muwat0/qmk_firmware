c7a56da200d9b2cd47faafa08375147cd5952d6e
murat <murat@muwat.org>
2026-07-09 15:46:19 +0300

    chore: cleanup

--- keyboards/gmmk/gmmk2/p96/iso/keymaps/muwat0/keymap.c
+++ keyboards/gmmk/gmmk2/p96/iso/keymaps/muwat0/keymap.c
@@ -21,12 +21,8 @@
 // Re-Positioning
 #define RE_CAPS KC_GESC // Caps Lock key acts as ESCAPE key
 
-// LAYER 2
-#define NU_LYER MO(_NU)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /* Keymap _BL: Base Layer (Default Layer)
-   */
+  /* Keymap _BL: Base Layer (Default Layer) */
 [_BL] = LAYOUT(
   KC_GESC,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_DEL,   KC_INS,   KC_PGUP,  KC_PGDN,
   KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_NLCK,  KC_PSLS,  KC_PAST,  KC_PMNS,
@@ -35,8 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    KC_P1,    KC_P2,    KC_P3,    KC_PENT,
   KC_LCTL,  KC_LGUI,  MO(_NU),                      KC_SPC,                               TG(_WASD),  MO(_FL),  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,    KC_PDOT),
 
-  /* Keymap _FL: Function Layer
-   */
+  /* Keymap _FL: Function Layer */
 [_FL] = LAYOUT(
     RESET,  KC_MYCM,  KC_WHOM,  KC_CALC,  KC_MSEL,  KC_MPRV,  KC_MRWD,  KC_MPLY,  KC_MSTP,  KC_MUTE,  KC_VOLU,  KC_VOLD,  _______,   _______,  _______,  _______,  _______,  _______,
   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,
